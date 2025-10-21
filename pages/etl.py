09e9129d860856812047447d8e3a934534c512a2
Markus Hoffmann <mhoffmann.aachen@gmail.com>
2025-10-22 00:00:10 +0200

    feat: Add link to Colab

--- pages/etl.py
+++ pages/etl.py
@@ -18,12 +18,19 @@ if selected_actual_file_name:
     # --- Streamlit App Layout ---
     with open(solution_path, "r") as file:
         content = file.read()
-        
-    st.download_button(
-        label="Download file",
-        data=content,
-        file_name=selected_actual_file_name.replace("_solutions", ""),
-        mime="application/x-ipynb+json"
-    )
+      
+    cols = st.columns(4)
+    with cols[0]:  
+        st.download_button(
+            label="Download notebook",
+            data=content,
+            file_name=selected_actual_file_name.replace("_solutions", ""),
+            mime="application/x-ipynb+json"
+        )
+    with cols[1]:
+        st.link_button(
+            label="Open in Google Colab",
+            url=f"https://colab.research.google.com/github/mhoffmannac/nfl-ds-challenges/blob/main/{ETL_FILES_SUBDIRECTORY}/{selected_actual_file_name}")
+
     display_ipynb_content(io.StringIO(content))
 
